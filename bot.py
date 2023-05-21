1e347f0c3d849165da2c59c9da912276b0f94a16
NoProtocolUnit <keith.and.kit@proton.me>
2023-05-21 01:36:13 -0600

    Switched to llama-cpp-python for the implementation.

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import requests
 import json
 import asyncio
 import httpx
+import random
 from aiohttp import ClientSession
 from discord.ext import commands
 from discord import app_commands
@@ -11,7 +12,7 @@ from discord import Interaction
 
 intents = discord.Intents.all()
 intents.message_content = True
-api_url = "http://localhost:5001/api/v1/"
+api_url = "http://localhost:8000/v1/"
 
 client = commands.Bot(command_prefix='$', intents=intents)
 
@@ -23,9 +24,14 @@ queue_to_process = asyncio.Queue()
 queue_to_send = asyncio.Queue()
 
 bot_name = "Night"
-bot_persona = "dry-humored, coffee-loving, sarcastic AI sidekick"
+bot_persona = "coffee-loving, sarcastic friend with a dry sense of humor"
 bot_gender = "male"
 bot_text_sample = "Night: Hello, what do you want?"
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
 
 # Create a character card that will be added to the prompt sent to the LLM.
 def get_character_card():
@@ -33,51 +39,53 @@ def get_character_card():
     # Your name is <name>.
     character_card = "Your name is " + bot_name + ". "
     
-    # Your name is <name>. You are a <persona> with human-equivalent emotions and opinions.
-    character_card = character_card + "You are a " + bot_persona + " with human-equivalent emotions and opinions. "
+    # Your name is <name>. You are a <persona> and have emotions and opinions.
+    character_card = character_card + "You are a " + bot_persona + " and have emotions and opinions. "
     
     # Your name is <name>. You are a <persona> with human-equivalent emotions and opinions. Your gender is <gender>. 
-    character_card = character_card + "Your gender is " + bot_gender + ". "
+    # character_card = character_card + "Your gender is " + bot_gender + ". "
 
     return character_card
         
 def create_prompt(message, author, character):
+
+    # Take the provided message and strip out @NightBot
     user_input = message.content.replace("<@1080950961268342874>","")
+    
+    # Remove any spaces before and after the input.
     user_input = user_input.strip()
-    add_to_context(user_input, author)
+    
+    # Create the prompt that will be sent in the prompt field.
     text = character + author + ": " + user_input + "\nNight: "
 
-    data ={}
-    data['n'] = 1
-    data['max_context_length'] = 512
-    data['max_length'] = 100
-    data['prompt'] = text
-    data['stop_sequence'] = [author+":", "Night:", "Day:", "\n\n"]
-    data['rep_pen'] = 1.1
-    data['temperature'] = 0.72
-    data['top_p'] = 0.73
-    data['top_k'] = 40
-    data['sampler_order'] = [5, 0, 2, 6, 3, 4, 1] 
+    # Make me a JSON file
+    data = {
+        "prompt": text,
+        "stop": [author+":", "Night:", "\n\n"],
+        "max_tokens": 100,
+        "user": author,
+        "temperature": 0.72,
+        "top_p": 0.73,
+        "top_k": 0,
+        "repeat_penalty": 1.08,
+        "n": 1,
+        "seed": 0,
+        "mirostat_mode": 2,
+        "mirostat_tau": 5.0,
+        "mirostat_eta": 0.2
+}
     
+    # Turn the thing into a JSON string and return it
     prompt = json.dumps(data)
     return prompt
   
 async def clean_reply(data, author):
         message = json.loads(data)
-        dirty_message = str(message['results'][0]['text'])
+        dirty_message = str(message['choices'][0]['text'])
         clean_message = dirty_message.replace(author + ":","")
         clean_message = clean_message.replace("\n\nNight:", "")
-        add_to_context (clean_message, "Night")
         return clean_message
  
-def add_to_context(data, author):
-    with open('context.txt', 'a', encoding="utf-8") as context:
-        context.write(author + ": " + data + "\n")
-        context.close()
-
-def read_context():
-    return context
-    
 def should_bot_reply(message):
     if message.author == client.user:
         return False
@@ -89,9 +97,10 @@ async def process_queue():
     while True:
         content = await queue_to_process.get()
         data = content[0]
-        print("Making API call.")
+        print("Sending" + data)
+        global headers
         async with ClientSession() as session:
-            async with session.post(api_url + "generate", data=data) as response:
+            async with session.post(api_url + "completions", headers=headers, data=data) as response:
                 response = await response.read()
                 queue_item = [response, content[1]]  # content[1] is the message
                 queue_to_send.put_nowait(queue_item)
@@ -100,7 +109,7 @@ async def process_queue():
 async def send_queue():
     while True:
         reply = await queue_to_send.get()
-        answer = await clean_reply(reply[0], str(reply[1].author))
+        answer = await clean_reply(reply[0], str(reply[1].author.name))
         await reply[1].channel.send(answer, reference=reply[1])   
         queue_to_send.task_done()
 
@@ -111,7 +120,7 @@ async def on_ready():
     
     # Attempt to connect to the Kobold CPP api and shutdown the bot if it's not up
     try: 
-        api_check = requests.get(api_url + "model")
+        api_check = requests.get(api_url + "models")
     except requests.exceptions.RequestException as e:
         print(f'LLM KoboldCPP api is not currently up. Shutting down the bot.')
         await client.close()
@@ -123,9 +132,9 @@ async def on_ready():
     asyncio.create_task(process_queue())
     asyncio.create_task(send_queue())
     
-    # Sync current slash commands
+    # Sync current slash commands (commented out unless we have new commands)
     client.tree.add_command(personality)
-    synced = await client.tree.sync()
+    await client.tree.sync()
    
 @client.event
 async def on_message(message):
@@ -135,7 +144,7 @@ async def on_message(message):
         character = get_character_card()
         
         # Create the JSON prompt to use
-        data = create_prompt(message, str(message.author), character)
+        data = create_prompt(message, str(message.author.name), character)
         
         # Add request to a queue to process
         queue_item = [data, message]
@@ -167,9 +176,10 @@ async def reset_personality(interaction):
             
     # Update the global variable
     old_personality = bot_persona
-    bot_persona = "dry-humored, coffee-loving, sarcastic AI sidekick"
+    bot_persona = "coffee-loving, sarcastic friend with a dry sense of humor"
         
     # Display new personality, so we know where we're at
     await interaction.response.send_message("Bot's personality has been updated from \"" + old_personality + "\" to \"" + bot_persona + "\".")
     
-client.run('KEY')
+client.run('API_KEY')
+
