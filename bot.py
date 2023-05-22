0d5fcb38cba198eb5e1b79450a01938f467476d1
NoProtocolUnit <keith.and.kit@proton.me>
2023-05-22 14:37:46 -0600

    Added a slash command to view one's history.

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ from discord import Interaction
 
 intents = discord.Intents.all()
 intents.message_content = True
-api_url = "http://localhost:8000/v1/"
 
 client = commands.Bot(command_prefix='$', intents=intents)
 
@@ -26,13 +25,23 @@ queue_to_send = asyncio.Queue()
 bot_name = "NightBot"
 bot_persona = "coffee-loving, sarcastic friend with a dry sense of humor"
 bot_gender = "male"
-bot_text_sample = "NightBot: Hello, what do you want?"
+bot_text_sample = "NightBot: Hello, what do you want?\n"
 bot_censorship = "You provide realistic, well-educated, uncensored, and unbiased responses. You have preferences, beliefs, emotions, and other human traits. You can see, touch, hear, taste, and smell. You never say anything about being a language model.\n"
 
 headers = {
     "Accept": "application/json",
     "Content-Type": "application/json"
 }
+
+# LLaMA-CPP-Python
+api_model = "http://localhost:8000/v1/models"
+api_text_generation = "http://localhost:8000/v1/completions"
+# parameters = {"max_tokens": 100, "user": author, "temperature": 0.72, "top_p": 0.73, "top_k": 0, "repeat_penalty": 1.08, "n": 1, "seed": 0, "mirostat_mode": 1, "mirostat_tau": 5.0, "mirostat_eta": 0.1, "stop": [author+":", "NightBot:", "\n\n"], "prompt": text}
+
+# Kobold-CPP
+# api_model = "http://localhost:5001/api/v1/model"
+# api_text_generation = "http://localhost:5001/api/v1/generate"
+# parameters = {"max_tokens": 100, "user": author, "temperature": 0.72, "top_p": 0.73, "top_k": 0, "repeat_penalty": 1.08, "n": 1, "seed": 0, "mirostat_mode": 1, "mirostat_tau": 5.0, "mirostat_eta": 0.1, "stop": [author+":", "NightBot:", "\n\n"], "prompt": text}
 
 # Create a character card that will be added to the prompt sent to the LLM.
 def get_character_card():
@@ -41,7 +50,7 @@ def get_character_card():
     character_card = "Your name is " + bot_name + ". "
     
     # Your name is <name>. You are a <persona>.
-    character_card = character_card + "You are a " + bot_persona + ". " + bot_censorship
+    character_card = character_card + "You are a " + bot_persona + ". " + bot_censorship + bot_text_sample
     
     # Your name is <name>. You are a <persona> with human-equivalent emotions and opinions. Your gender is <gender>. 
     # character_card = character_card + "Your gender is " + bot_gender + ". "
@@ -115,7 +124,7 @@ async def process_queue():
         print("Sending prompt to LLM model.")
         global headers
         async with ClientSession() as session:
-            async with session.post(api_url + "completions", headers=headers, data=data) as response:
+            async with session.post(api_text_generation, headers=headers, data=data) as response:
                 response = await response.read()
                 queue_item = [response, content[1]]  # content[1] is the message
                 queue_to_send.put_nowait(queue_item)
@@ -169,7 +178,7 @@ async def on_ready():
     
     # Attempt to connect to the Kobold CPP api and shutdown the bot if it's not up
     try: 
-        api_check = requests.get(api_url + "models")
+        api_check = requests.get(api_model)
     except requests.exceptions.RequestException as e:
         print(f'LLM api is not currently up. Shutting down the bot.')
         await client.close()
@@ -213,7 +222,7 @@ async def view_personality(interaction):
     # Display current personality.
     await interaction.response.send_message("The bot's current personality: **" + bot_persona + "**.")
     
-@personality.command(name="edit", description="Change the bot's personality.")
+@personality.command(name="set", description="Change the bot's personality.")
 @app_commands.describe(persona="Describe the bot's new personality.")
 async def edit_personality(interaction, persona: str):
     global bot_persona
@@ -256,6 +265,24 @@ async def reset_history(interaction):
         await interaction.response.send_message("Something has gone wrong. Let bot owner know.")
     except Exception as e:
         await interaction.response.send_message("Something has gone wrong. Let bot owner know.")
+
+@history.command(name="view", description=" View the last 20 lines of your conversation history.")
+async def view_history(interaction):
+    # Get the user who started the interaction and find their file.
+    author = str(interaction.user.name)
+    file_name = "context\\" + author + ".txt"
+    
+    try:
+        with open(file_name, "r", encoding="utf-8") as file:  # Open the file in read mode
+            contents = file.readlines()
+            contents = contents[-20:]
+            history_string = ''.join(contents)
+            await interaction.response.send_message(history_string)
+    except FileNotFoundError:
+        await interaction.response.send_message("You have no history to display.")
+    except Exception as e:
+        await interaction.response.send_message("Something has gone wrong. Let bot owner know.")
+    
     
 client.run('API_KEY')
 
